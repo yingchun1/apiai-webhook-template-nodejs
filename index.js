267795f154856c026ebced746beed18f0590a8f4
Yingchun Zhang <200zyc@163.com>
2017-05-26 13:34:13 +0800

    22

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').ApiAiApp;
 
 // [START YourAction]
-exports.yourAction = (request, response) => {
+exports.join.meeting = (request, response) => {
   const app = new App({request, response});
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
